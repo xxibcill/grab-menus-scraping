3734b650d95ae77c2bc580ff1ca4b0afaf44d584
xxibcill <36674072+xxibcill@users.noreply.github.com>
2023-09-09 01:14:49 +0700

    init project

---
+++ index.js
@@ -0,0 +1,86 @@
+import puppeteer from 'puppeteer'
+import fs from 'fs'
+import { urls } from './constant.js'
+
+let page
+
+const autoScroll = async () => {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      var totalHeight = 0
+      var distance = 1000
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight
+        window.scrollBy(0, distance)
+        totalHeight += distance
+
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer)
+          resolve()
+        }
+      }, 1000)
+    })
+  })
+}
+
+const getShopName = async () => {
+  await page.waitForSelector(`[class*='name']`)
+
+  const shopNameEl = await page.$(`[class*='name']`)
+
+  const shopName = await page.evaluate((el) => el.textContent, shopNameEl)
+  console.log(shopName)
+}
+
+const getAllMenu = async () => {
+  const root = await page.$(`[class*='contentWrapper']`)
+
+  await page.waitForSelector(`[class*='name']`)
+
+  await page.waitForSelector(`[class*='menuItem'].ant-col-md-24`)
+
+  await autoScroll()
+
+  const menus = await page.evaluate((el) => {
+    const temp = []
+    let menuItems = Array.from(el.querySelectorAll(`[class*='menuItem'].ant-col-md-24`))
+
+    menuItems.forEach((menuItem) => {
+      const title = menuItem.querySelector(`[class*='itemNameTitle']`).textContent
+      const description = menuItem.querySelector(`[class*='itemDescription']`)
+      const imageSrc = menuItem.querySelector(`[class*='menuItemPhoto'] img`).src
+      const price = menuItem.querySelector(`[class*='discountedPrice']`).textContent
+
+      temp.push({
+        title,
+        description: description ? description.textContent : '',
+        imageSrc,
+        price,
+      })
+    })
+
+    return temp
+  }, root)
+
+  console.log(menus.length)
+  fs.writeFileSync('data.json', JSON.stringify(menus))
+}
+
+;(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreHTTPSErrors: true,
+    args: [`--window-size=1920,1080`],
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+  })
+
+  page = await browser.newPage()
+  await page.goto(urls[0])
+
+  await getAllMenu()
+
+  await browser.close()
+})()
